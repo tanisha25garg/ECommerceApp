f5d3c934c43cff7f18c25fddf0454c4762263e1a
Tanisha Garg <tanishagarg2503@gmail.com>
2025-03-02 15:02:20 +0530

    Create the footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                At Forever, we are committed to providing you with top-quality products and a seamless shopping experience. 
+                Explore our latest collections, enjoy exclusive deals, and shop with confidence knowing that we prioritize your satisfaction.
+            </p>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+91-9350093500</li>
+                <li>contact@foreveryou.com</li>
+            </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025@ forever.com - All Right Reserved.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
