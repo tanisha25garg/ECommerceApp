11ffc156614f8275cd0604df79fb671ca757fb70
Tanisha Garg <tanishagarg2503@gmail.com>
2025-03-05 13:16:21 +0530

    Create mongodb and cloudinary config files

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 // App Config
 const app = express()
@@ -9,6 +11,8 @@ const port = process.env.PORT || 4000
 // Middlewares
 app.use(express.json())
 app.use(cors())
+connectDB()
+connectCloudinary()
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send('Hello World'))
